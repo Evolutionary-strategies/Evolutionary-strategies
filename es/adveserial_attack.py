e0f745804ce9380894c30d7245562de0f7efbbf5
Robin Christoffer Vold <robvold98@gmail.com>
2022-01-27 12:29:36 +0100

    Fiksa på angrep. Skal være mer eller mindre ferdig nå

--- es/adveserial_attack.py
+++ es/adveserial_attack.py
@@ -2,28 +2,49 @@
 from util import load_data
 from model import load_model
 import foolbox as fb
+import numpy as np
 
 loader = load_data()
 trainloader = loader[0]
 testloader = loader[1]
 
-model = load_model()
-fmodel = fb.PyTorchModel(model, bounds=(0,1))
-attack = fb.attacks.LinfFastGradientAttack()
+# Kjører kun på en CPU
+class Attack():
+    def __init__(self, model = None) -> None:
+        if model is None:
+            model = load_model()
+        self.fmodel = fb.PyTorchModel(model, bounds=(0,1))
+
+    def default_accuracy(self) -> float:
+        accuracy = 0
+        for i, data in enumerate(testloader, 0):
+            images, labels = data
+            accuracy += fb.utils.accuracy(self.fmodel, images, labels)
+        return accuracy/i
+    
+    def perform(self, attack, epsilons = [0.03]) -> list[float]:
+        accuracy = []
+        for eps in epsilons:
+            acc = 0
+            for i, data in enumerate(testloader, 0):
+                images, labels = data
+                raw, clipped, is_adv = attack(self.fmodel, images, labels, epsilons=eps)
+                acc += 1 - is_adv.float().mean(axis=-1)
+            accuracy.append(acc.item()/i)
+        return accuracy
+
+    def perform_attacks(self, attacks, epsilons = [0.03]) -> dict[list[float]]:
+        results = {}
+        for attack in attacks:
+            results[attack] = self.perform(attack, epsilons)
+        return results
+            
 
 
 
-accuracy = 0
-robust_accuracy = 0
-i = 0
-for data in testloader:
-    images, labels = data
-    accuracy += fb.utils.accuracy(fmodel, images, labels)
 
-    raw, clipped, is_adv = attack(fmodel, images, labels, epsilons=0.03)
-    robust_accuracy += 1 - is_adv.float().mean(axis=-1)
 
-    i += 1
-
-print(accuracy/i)
-print(robust_accuracy.item()/i)
+a = Attack()
+# print(a.default_accuracy())
+print(a.perform(fb.attacks.LinfFastGradientAttack(), np.linspace(0.0, 0.1, num=10)))
+# print(a.perform_attacks([fb.attacks.LinfFastGradientAttack(), fb.attacks.L2FastGradientAttack()]))
